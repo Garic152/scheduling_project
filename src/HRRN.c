33c5513a26fd3e6a981c9063ee2e5665db487d44
Linux <erictheiler15@gmail.com>
2024-06-06 15:28:42 +0200

    fix: partially fixed HRRN, fails on edge cases

--- src/HRRN.c
+++ src/HRRN.c
@@ -12,32 +12,42 @@ float response_ratio(process *p) {
   }
   // Calculates the response ratio of a process
   int waiting_time = current_time - p->start_time;
+
+  if (p->time_left == 0) {
+    return 99999.99;
+  }
+
   return (waiting_time + p->time_left) / (float)p->time_left;
 }
 
 void HRRN_sort_queue() {
-  // Updates the second last element in the queue to the process with the lowest
-  // respones_ratio
-  if (HRRN_queue->next == NULL) {
+  if (HRRN_queue->next == NULL || HRRN_queue->next->next == NULL) {
     return;
   }
 
-  queue_object *current = HRRN_queue->next;
   queue_object *prev = HRRN_queue;
-  queue_object *lowest_node = HRRN_queue->next;
-  queue_object *lowest_prev = HRRN_queue;
+  queue_object *current = HRRN_queue->next; // Start from the second element
+  queue_object *nextNode = HRRN_queue;
 
+  // Traverse the queue until the last node
   while (current->next != NULL) {
-    if (response_ratio(current->object) < response_ratio(lowest_node->object)) {
-      lowest_node = current;
-      lowest_prev = prev;
-    }
-    prev = current;
-    current = current->next;
-  }
+    nextNode = current->next;
 
-  lowest_prev->next = current;
-  prev->next = lowest_node;
+    // If the response ratio of the current node is greater than the next node
+    if (response_ratio(current->object) > response_ratio(nextNode->object)) {
+      // Move the current node to the end of the queue
+      prev->next = nextNode;
+      current->next = nextNode->next;
+      nextNode->next = current;
+
+      // Update the pointers for the next iteration
+      prev = nextNode;
+    } else {
+      // Move to the next pair of nodes
+      prev = current;
+      current = nextNode;
+    }
+  }
 }
 
 process *HRRN_tick(process *running_process) {
@@ -63,8 +73,8 @@ int HRRN_startup() {
 
 process *HRRN_new_arrival(process *arriving_process, process *running_process) {
   if (arriving_process != NULL) {
-    printf("\nProcess %c arrived at time %d\n", arriving_process->id,
-           current_time);
+    // printf("\nProcess %c arrived at time %d\n", arriving_process->id,
+    // current_time);
     queue_add(arriving_process, HRRN_queue);
   }
   return running_process;
