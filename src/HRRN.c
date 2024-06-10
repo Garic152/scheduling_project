4ee45c231ef05c11b8f9e956930f89886c94164d
Linux <erictheiler15@gmail.com>
2024-06-10 18:47:25 +0200

    new tests and maybe working HRRN somehow still prints weird RR

--- src/HRRN.c
+++ src/HRRN.c
@@ -14,6 +14,26 @@ static queue_object *HRRN_queue;
 // You can add more global variables and structs here
 int current_time = 0;
 
+void print_queue() {
+  // printf("\n----------------\n");
+  // printf("Printing Queue\n");
+
+  if (HRRN_queue->next == NULL) {
+    // printf("Empty Queue\n");
+    // printf("----------------\n");
+    return;
+  }
+
+  queue_object *iterator = HRRN_queue->next;
+
+  while (iterator != NULL) {
+    // printf("%c, %d\n", ((process *)(iterator->object))->id,
+    //        ((process *)(iterator->object))->time_left);
+    iterator = iterator->next;
+  }
+  // printf("----------------\n");
+}
+
 float response_ratio(process *p) {
   // Calculates the response ratio of a process
   if (p == NULL) {
@@ -34,12 +54,11 @@ float response_ratio(process *p) {
     }
   }
   if (init_time == -1) {
-    fprintf(stderr, "Did not find arrival time for process ID: %c", p->id);
+    // fprintf(stderr, "Did not find arrival time for process ID: %c", p->id);
   }
 
   int waiting_time =
       (current_time - p->start_time) - (init_time - p->time_left);
-
   printf("Response-Ratio for process %c is: %.2f\n", p->id,
          (waiting_time + p->time_left) / (float)p->time_left);
 
@@ -55,23 +74,26 @@ void HRRN_sort_queue() {
   queue_object *current = HRRN_queue->next; // Start from the second element
   queue_object *nextNode = HRRN_queue;
 
-  // Traverse the queue until the last node
   while (current->next != NULL) {
     nextNode = current->next;
 
-    // If the response ratio of the current node is greater than the next node
-    printf("Comparing node %c and %c\n", ((process *)(current->object))->id,
-           ((process *)(nextNode->object))->id);
     if (response_ratio(current->object) > response_ratio(nextNode->object)) {
-      // Move the current node to the end of the queue
       prev->next = nextNode;
       current->next = nextNode->next;
       nextNode->next = current;
 
-      // Update the pointers for the next iteration
       prev = nextNode;
+    } else if (response_ratio(current->object) ==
+               response_ratio(nextNode->object)) {
+      if (((process *)(current->object))->start_time >
+          ((process *)(nextNode->object))->start_time) {
+        prev->next = nextNode;
+        current->next = nextNode->next;
+        nextNode->next = current;
+
+        prev = nextNode;
+      }
     } else {
-      // Move to the next pair of nodes
       prev = current;
       current = nextNode;
     }
@@ -79,6 +101,7 @@ void HRRN_sort_queue() {
 }
 
 process *HRRN_tick(process *running_process) {
+  print_queue();
   current_time += 1;
   if (running_process == NULL || running_process->time_left == 0) {
     HRRN_sort_queue();
@@ -99,17 +122,17 @@ int HRRN_startup() {
 }
 
 process *HRRN_new_arrival(process *arriving_process, process *running_process) {
-  printf("\n\nTick %d\n", current_time);
+  // printf("\n\nTick %d\n", current_time);
 
   if (arriving_process != NULL) {
-    printf("Process %c arrived\n", arriving_process->id);
+    // printf("Process %c arrived\n", arriving_process->id);
 
     if (process_count < MAX_PROCESSES) {
       process_info_array[process_count].id = arriving_process->id;
       process_info_array[process_count].time = arriving_process->time_left;
       process_count += 1;
     } else {
-      fprintf(stderr, "Process info array is full!\n");
+      // fprintf(stderr, "Process info array is full!\n");
     }
 
     queue_add(arriving_process, HRRN_queue);
