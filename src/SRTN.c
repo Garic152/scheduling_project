d2181761666efba71d724dc381f33ed11ced35d2
Linux <erictheiler15@gmail.com>
2024-06-12 13:53:25 +0200

    feat: added SRTN

--- src/SRTN.c
+++ src/SRTN.c
@@ -1,27 +1,77 @@
 #include "../lib/SRTN.h"
 
 static queue_object *SRTN_queue;
-//You can add more global variables here
+// You can add more global variables here
 
-process *SRTN_tick(process *running_process)
-{
-	// TODO
-	return NULL;
+void SRTN_sort_queue() {
+  if (SRTN_queue->next == NULL || SRTN_queue->next->next == NULL) {
+    return;
+  }
+
+  queue_object *prev = SRTN_queue;
+  queue_object *current = SRTN_queue->next; // Start from the second element
+  queue_object *nextNode = SRTN_queue;
+
+  while (current->next != NULL) {
+    nextNode = current->next;
+
+    if (((process *)(current->object))->time_left <
+        ((process *)(nextNode->object))->time_left) {
+      prev->next = nextNode;
+      current->next = nextNode->next;
+      nextNode->next = current;
+
+      prev = nextNode;
+    } else {
+      prev = current;
+      current = nextNode;
+    }
+  }
 }
 
-int SRTN_startup()
-{
-	// TODO
-	return 1;
+process *SRTN_tick(process *running_process) {
+  if (running_process == NULL || running_process->time_left == 0) {
+    SRTN_sort_queue();
+    running_process = queue_poll(SRTN_queue);
+    if (running_process == NULL) {
+      return NULL;
+    }
+  }
+  if (running_process->time_left > 0) {
+    running_process->time_left -= 1;
+  }
+  return running_process;
 }
 
-process *SRTN_new_arrival(process *arriving_process, process *running_process)
-{
-	// TODO
-	return NULL;
+int SRTN_startup() {
+  SRTN_queue = new_queue();
+  if (SRTN_queue == NULL) {
+    return 1;
+  }
+  return 0;
 }
 
-void SRTN_finish()
-{
-	// TODO
+process *SRTN_new_arrival(process *arriving_process, process *running_process) {
+  if (arriving_process == NULL) {
+    return running_process;
+  }
+
+  if (running_process == NULL) {
+    return arriving_process;
+  }
+
+  if (arriving_process->time_left < running_process->time_left) {
+    queue_add(running_process, SRTN_queue);
+    return arriving_process;
+  } else {
+    queue_add(arriving_process, SRTN_queue);
+    return running_process;
+  }
+}
+
+void SRTN_finish() {
+  if (SRTN_queue != NULL) {
+    free_queue(SRTN_queue);
+    SRTN_queue = NULL;
+  }
 }
