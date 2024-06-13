23a916d66e672d158c10d07d23960bdd845224a8
Linux <erictheiler15@gmail.com>
2024-06-13 11:31:20 +0200

    feat: added RR chore: cleaned up prints

--- src/HRRN.c
+++ src/HRRN.c
@@ -1,5 +1,4 @@
 #include "../lib/HRRN.h"
-#include <stdio.h>
 
 typedef struct {
   char id;
@@ -14,32 +13,8 @@ static queue_object *HRRN_queue;
 // You can add more global variables and structs here
 int current_time = 0;
 
-void print_queue() {
-  // printf("\n----------------\n");
-  // printf("Printing Queue\n");
-
-  if (HRRN_queue->next == NULL) {
-    // printf("Empty Queue\n");
-    // printf("----------------\n");
-    return;
-  }
-
-  queue_object *iterator = HRRN_queue->next;
-
-  while (iterator != NULL) {
-    // printf("%c, %d\n", ((process *)(iterator->object))->id,
-    //        ((process *)(iterator->object))->time_left);
-    iterator = iterator->next;
-  }
-  // printf("----------------\n");
-}
-
 float response_ratio(process *p) {
   // Calculates the response ratio of a process
-  if (p == NULL) {
-    fprintf(stderr, "Trying to access process NULL pointer");
-    exit(EXIT_FAILURE);
-  }
 
   if (p->time_left == 0) {
     return 99999.99;
@@ -54,13 +29,9 @@ float response_ratio(process *p) {
     }
   }
   if (init_time == -1) {
-    // fprintf(stderr, "Did not find arrival time for process ID: %c", p->id);
   }
   int start_time = p->start_time + 1;
-  int waiting_time =
-      (current_time - start_time) - (init_time - p->time_left);
-  printf("Response-Ratio for process %c is: %.2f\n", p->id,
-         (waiting_time + p->time_left) / (float)p->time_left);
+  int waiting_time = (current_time - start_time) - (init_time - p->time_left);
 
   return ((waiting_time + p->time_left) / (float)p->time_left);
 }
@@ -104,7 +75,6 @@ void HRRN_sort_queue() {
 }
 
 process *HRRN_tick(process *running_process) {
-  print_queue();
   current_time += 1;
   if (running_process == NULL || running_process->time_left == 0) {
     HRRN_sort_queue();
@@ -125,17 +95,13 @@ int HRRN_startup() {
 }
 
 process *HRRN_new_arrival(process *arriving_process, process *running_process) {
-  // printf("\n\nTick %d\n", current_time);
-
   if (arriving_process != NULL) {
-    // printf("Process %c arrived\n", arriving_process->id);
 
     if (process_count < MAX_PROCESSES) {
       process_info_array[process_count].id = arriving_process->id;
       process_info_array[process_count].time = arriving_process->time_left;
       process_count += 1;
     } else {
-      // fprintf(stderr, "Process info array is full!\n");
     }
 
     queue_add(arriving_process, HRRN_queue);
--- src/RR.c
+++ src/RR.c
@@ -1,27 +1,79 @@
 #include "../lib/RR.h"
+#include <stdio.h>
 
 static queue_object *RR_queue;
-//You can add more global variables
+int time = 0;
+int RR;
+// You can add more global variables
 
-process *RR_tick(process *running_process)
-{
-	// TODO
-	return NULL;
+void RR_update_queue() {
+  if (RR_queue->next == NULL || RR_queue->next->next == NULL) {
+    return;
+  }
+
+  queue_object *current = RR_queue->next; // Start from the second element
+  queue_object *last = RR_queue->next;
+
+  while (last->next != NULL) {
+    last = last->next;
+  }
+
+  RR_queue->next = current->next;
+  last->next = current;
+  current->next = NULL;
 }
 
-int RR_startup(int quantum)
-{
-	// TODO
-	return 1;
+process *RR_select_next() {
+  if (RR_queue->next == NULL) {
+    return NULL;
+  }
+  return RR_queue->next->object;
 }
 
-process *RR_new_arrival(process *arriving_process, process *running_process)
-{
-	// TODO
-	return NULL;
+process *RR_tick(process *running_process) {
+  if (running_process == NULL || running_process->time_left == 0) {
+    time = 0;
+    running_process = queue_poll(RR_queue);
+    if (running_process == 0) {
+      return NULL;
+    }
+  }
+
+  if (time >= RR) {
+    time = 0;
+    queue_add(running_process, RR_queue);
+    running_process = queue_poll(RR_queue);
+  }
+
+  running_process->time_left -= 1;
+  time += 1;
+
+  return running_process;
 }
 
-void RR_finish()
-{
-	// TODO
+int RR_startup(int quantum) {
+  RR_queue = new_queue();
+  RR = quantum;
+  if (RR_queue == NULL) {
+    return 1;
+  }
+  return 0;
+}
+
+process *RR_new_arrival(process *arriving_process, process *running_process) {
+  if (arriving_process != NULL) {
+    if (running_process == NULL) {
+      return arriving_process;
+    } else {
+      queue_add(arriving_process, RR_queue);
+    }
+  }
+  return running_process;
+}
+
+void RR_finish() {
+  if (RR_queue != NULL) {
+    free_queue(RR_queue);
+    RR_queue = NULL;
+  }
 }
